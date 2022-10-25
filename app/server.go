629fb060278cb665db7408427752d6f8b42cc96d
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2022-10-25 21:14:24 +0200

    stage2

--- app/server.go
+++ app/server.go
@@ -10,16 +10,27 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-  l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
-  if err != nil {
-    fmt.Println("Failed to bind to port 6379")
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
 
-  _, err = l.Accept()
-  if err != nil {
-    fmt.Println("Error accepting connection: ", err.Error())
-    os.Exit(1)
-  }
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+	}
+
+	if string(buf) == "PING" {
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
