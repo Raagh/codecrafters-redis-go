0b25a6f68bf42c405e3e7d0add4c76de42174d78
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2022-10-26 21:49:55 +0200

    read multiple times

--- app/server.go
+++ app/server.go
@@ -7,8 +7,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
 	if err != nil {
@@ -16,13 +14,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
+	for {
 		buf := make([]byte, 1024)
 
 		_, err = conn.Read(buf)
