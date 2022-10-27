b6779cc8454a37e751f1b569bdafb524203cdb36
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2022-10-27 21:32:02 +0200

    revert

--- app/server.go
+++ app/server.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -30,26 +29,13 @@ func handle(conn net.Conn) {
 	for {
 		buf := make([]byte, 1024)
     
-		if _, err := conn.Read(buf); err != nil {
-			// fmt.Println("Error reading: ", err.Error())
+		if _, err := conn.Read([]byte{}); err != nil {
+			fmt.Println("Error reading: ", err.Error())
       continue;
 		}
 
-    message := string(buf)
+    fmt.Println(string(buf))
 
-    if message == "" {
-      continue
-    }
-
-    // splitMessage := strings.Split(message, "\r\n")
-    fmt.Println(message)
-
-    if strings.ToLower(message) == "ping" {
-      fmt.Println("PONG")
-      conn.Write([]byte("+PONG\r\n"))
-    } else {
-      // numberOfParameters := splitMessage[0][1:]
-      // fmt.Println(numberOfParameters)
-    }
+    conn.Write([]byte("+PONG\r\n"))
 	}
 }
